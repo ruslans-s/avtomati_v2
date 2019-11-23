891d067479dae4569eecdb98c943c92951e1e57e
RS <51824085+ruslans-s@users.noreply.github.com>
2019-11-24 01:18:56 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include<QLabel>
+#include <QApplication>
+#include "generator.h"
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+   // generator generator;
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
